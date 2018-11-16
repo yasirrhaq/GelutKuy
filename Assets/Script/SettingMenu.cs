998dc02efe1fbe6baf72bc1e30fbdb417df51d35
Yasir Haq <yasir.haq98@gmail.com>
2018-11-16 22:52:11 +0700

    Add Setting Menu Script

---
+++ Assets/Script/SettingMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingMenu : MonoBehaviour {
+
+    public Slider volume;
+    public AudioSource audioSource;
+
+    public void SetVolume()
+    {
+        audioSource.volume = volume.value;
+    }
+
+    private void Update()
+    {
+        SetVolume();
+    }
+}
