9508b444d6c15a68f4e9d6a462c3a9b096668cb9
Yasir Haq <yasir.haq98@gmail.com>
2018-11-16 21:17:58 +0700

    GelutKuy

---
+++ Assets/Script/ButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+    public void Play(int index)
+    {
+        SceneManager.LoadSceneAsync(index);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Berhasil Exit");
+    }
+}
