96a5f8be62d9558d66756ea693004f7d4ae59919
Yasir Haq <yasir.haq98@gmail.com>
2018-11-27 15:50:34 +0700

    Add Character Selection

--- Assets/Script/Character/CharacterManager.cs
+++ Assets/Script/Character/CharacterManager.cs
@@ -5,67 +5,65 @@ using UnityEngine.SceneManagement;
 
 public class CharacterManager : MonoBehaviour {
 
-    public GameObject[] listChar;
+    public List<GameObject> listCharacter;
+    public List<GameObject> characters;
+    public Transform characterPos;
     private int index;
 
     private void Start()
     {
-        listChar = new GameObject[transform.childCount];
+        characters = new List<GameObject>();
+        GetCharacter();
+        index = 0;
+        HideCharacter();
 
-        // Isi array character
-        for(int i = 0; i < transform.childCount; i++)
+        characters[0].SetActive(true);
+        
+    }
+    
+    private void GetCharacter()
+    {
+        foreach (GameObject character in listCharacter)
         {
-            listChar[i] = transform.GetChild(i).gameObject;
-        }
-
-        // hide character
-        foreach (GameObject go in listChar)
-        {
-            go.SetActive(false);
-        }
-        // activer character ter awal
-        if (listChar[0])
-        {
-           listChar[0].SetActive(true);
+            characters.Add(Instantiate(character, characterPos.position, Quaternion.identity));
         }
     }
 
-    public void PanahKiri()
+    private void HideCharacter()
     {
-        // non aktif current char
-        listChar[index].SetActive(false);
+        foreach (GameObject character in characters)
+        {
+            character.SetActive(false);
+        }
+    }
 
-
+    public void SelectPrev()
+    {
+        characters[index].SetActive(false);
         index--;
-        if (index < 0 )
-        {
-            index = listChar.Length - 1;
-        }
 
-        // aktif current char
-        listChar[index].SetActive(true);
+        if (index < 0)
+        {
+            index = characters.Count - 1;
+        }
+        characters[index].SetActive(true);
     }
 
-    public void PanahKanan()
+    public void SelectNext()
     {
-        // non aktif current char
-        listChar[index].SetActive(false);
-
-
+        characters[index].SetActive(false);
         index++;
-        if (index == listChar.Length)
-        {
-            index = 0
-;
-        }
 
-        // aktif current char
-        listChar[index].SetActive(true);
+        if (index == characters.Count)
+        {
+            index = 0;
+        }
+        characters[index].SetActive(true);
     }
 
     public void Confirm()
     {
-        PlayerPrefs.SetInt("CharacterSelected", index);
-        SceneManager.LoadScene("BattleScene");
+        PlayerPrefs.SetInt("Character Selected", index);
+        SceneManager.LoadSceneAsync("BattleScene");
     }
 }
