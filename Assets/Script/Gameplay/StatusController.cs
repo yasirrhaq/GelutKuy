4c352d9fa77e9225e041acdd4f958b7820ca0884
Yasir Haq <yasir.haq98@gmail.com>
2018-12-18 18:27:28 +0700

    Add StatusController Script

---
+++ Assets/Script/Gameplay/StatusController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatusController : MonoBehaviour {
+
+    public Hand player;
+    public Deck deck;
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI deckText;
+    
+    public void UpdateHealthUI()
+    {
+        healthText.text = "HP: " + player.health;
+    }
+
+    public void UpdateDeckUI()
+    {
+        if (!player.recycled)
+        {
+            deckText.text = "Deck: " + deck.cardIds.Count;
+        }
+        else
+        {
+            deckText.text = "Deck: " + deck.graveCardControllers.Count;
+        }
+    }
+}
