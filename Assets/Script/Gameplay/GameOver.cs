4bfc2e24daa02bdbb5ece0b36fcc4ba0c302564d
Yasir Haq <yasir.haq98@gmail.com>
2018-12-18 18:50:30 +0700

    Add GameOver Script

---
+++ Assets/Script/Gameplay/GameOver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+
+    public bool gameOver;
+    public Image result;
+    public Sprite winSprite;
+    public Sprite loseSprite;
+
+	public void GameOverCondition(bool win)
+    {
+        result.enabled = true;
+        if (win)
+        {
+            result.sprite = winSprite;
+        }
+        else
+        {
+            result.sprite = loseSprite;
+        }
+        gameOver = true;
+    }
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
