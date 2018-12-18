fd791bdbfad7fec0ab98532fc1f4d63004c69b5d
Yasir Haq <yasir.haq98@gmail.com>
2018-12-18 18:27:17 +0700

    Add Graveyard Script

---
+++ Assets/Script/Gameplay/Graveyard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graveyard : MonoBehaviour {
+
+    public Deck deck;
+    public Hand hand;
+    public List<CardController> usedCardControllers;
+    public StatusController status;
+
+    public void RecycleCards()
+    {
+        foreach (CardController usedCard in usedCardControllers)
+        {
+            usedCard.transform.SetParent(deck.transform);
+            usedCard.moving = true;
+            usedCard.enabled = true;
+            usedCard.FlipCard(false);
+
+            deck.graveCardControllers.Add(usedCard);
+        }
+
+        usedCardControllers.RemoveRange(0, usedCardControllers.Count);
+
+        deck.ShuffleController();
+        hand.recycled = true;
+        status.UpdateDeckUI();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            RecycleCards();
+        }
+    }
+}
